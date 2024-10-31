807c3273ddb833b30142eb09ffb8bbad55da0839
Pattadon Udompaipeuk <pattadon.u@ku.th>
2024-10-31 16:03:48 +0700

    Procedural programming for data processing

--- data_processing.py
+++ data_processing.py
@@ -20,7 +20,7 @@ print("The average temperature of all the cities:")
 temps = []
 for city in cities:
     temps.append(float(city['temperature']))
-print(sum(temps)/len(temps))
+print(sum(temps) / len(temps))
 print()
 
 # Print all cities in Italy
@@ -40,7 +40,7 @@ for city in cities:
     if city['country'] == my_country:
         temps.append(float(city['temperature']))
 print("The average temperature of all the cities in", my_country, ":")
-print(sum(temps)/len(temps))
+print(sum(temps) / len(temps))
 print()
 
 # Print the max temperature for all the cities in Italy
@@ -63,8 +63,56 @@ print("The min temperature of all the cities in", my_country, ":")
 print(min(temps))
 print()
 
+
+# Let's write a function to filter out only items that meet the condition
+# Hint: condition will be associated with an anonymous function, e.x., lamdbda x: max(x)
+def filter(condition, dict_list):
+    filtered_list = []
+    for item in dict_list:
+        if condition(item):
+            filtered_list.append(item)
+    return filtered_list
+
+
+x = filter(lambda x: float(x['latitude']) >= 60.0, cities)
+for item in x:
+    print(item)
+
+
+# Let's write a function to do aggregation given an aggregation function and an aggregation key
+def aggregate(aggregation_key, aggregation_function, dict_list):
+    temp = []
+    for item in dict_list:
+        temp.append(float(item[aggregation_key]))
+    return aggregation_function(temp)
+
+
 # Let's write code to
 # - print the average temperature for all the cities in Italy
+my_country = 'Italy'
+print("The average temperature of all the cities in", my_country, ":")
+print(aggregate('temperature', lambda x: sum(x)/len(x),
+                filter(lambda x: x['country'] == my_country, cities)))
+print()
+
 # - print the average temperature for all the cities in Sweden
+my_country = 'Sweden'
+print("The average temperature of all the cities in", my_country, ":")
+print(aggregate('temperature', lambda x: sum(x)/len(x),
+                filter(lambda x: x['country'] == my_country, cities)))
+print()
+
 # - print the min temperature for all the cities in Italy
-# - print the max temperature for all the cities in Sweden
+my_country = 'Italy'
+print("The average temperature of all the cities in", my_country, ":")
+print(aggregate('temperature', lambda x: min(x),
+                filter(lambda x: x['country'] == my_country, cities)))
+print()
+
+# - print the max ,temperature for all the cities in Sweden
+my_country = 'Sweden'
+print("The average temperature of all the cities in", my_country, ":")
+print(aggregate('temperature', lambda x: max(x),
+                filter(lambda x: x['country'] == my_country, cities)))
+print()
+
