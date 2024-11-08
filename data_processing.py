694eaa29d8677a86da08567b12e5af9be9eb261a
Pattadon Udompaipeuk <pattadon.u@ku.th>
2024-11-08 14:18:51 +0700

    data processing with oop style with test case

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,20 @@
+import csv, os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+cities = []
+with open(os.path.join(__location__, 'Cities.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        cities.append(dict(r))
+
+countries = []
+with open(os.path.join(__location__, 'Countries.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        countries.append(dict(r))
+
 class TableDB:
     def __init__(self):
         self.table_database = []
@@ -6,9 +23,9 @@ class TableDB:
         self.table_database.append(table)
 
     def search(self, table_name):
-        for i in range(len(self.table_database)):
-            if self.table_database[i].table_name == table_name:
-                return i
+        for table in self.table_database:
+            if table.table_name == table_name:
+                return table
         return None
 
 class Table:
@@ -32,15 +49,33 @@ class Table:
         return aggregation_function(col_val) if col_val else None
 
     def __str__(self):
-        f"{self.table_name} : {self.table}"
+        return f"{self.table_name} : {self.table}"
 
+#Print the min and max temperatures for cities in EU that do not have coastlines
+eu_country_items = []
+eu_no_coastline = filter(lambda x: x["coastline"] == "no" and x["EU"] == "yes", countries)
+for item in eu_no_coastline:
+    eu_country_items.append(item["country"])
 
+temp = []
+cities_eu = filter(lambda x: x["country"], cities)
+for i in cities_eu:
+    if i["country"] in eu_country_items:
+        temp.append(float(i["temperature"]))
 
+min_temp_eu_no_coastline = min(temp)
+max_temp_eu_no_coastline = max(temp)
+print(f"The min temperatures for cities in EU that do not have coastlines : {min_temp_eu_no_coastline}")
+print(f"The max temperatures for cities in EU that do not have coastlines : {max_temp_eu_no_coastline}")
+print()
 
+#Print the min and max latitude for cities in every country
+latitude_list = []
+a = filter(lambda x: x["latitude"], cities)
+for item in a:
+    latitude_list.append(float(item["latitude"]))
 
-
-
-
-
-
-
+min_latitude = min(latitude_list)
+max_latitude = max(latitude_list)
+print(f"The min latitude for cities in every country : {min_latitude}")
+print(f"The max latitude for cities in every country : {max_latitude}")
